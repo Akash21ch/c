66f2c47ccd378800fddc77442d3d334b0f2755a2
Akash21ch <109484103+Akash21ch@users.noreply.github.com>
2024-03-23 12:25:14 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function learnMore(category) {
+  // Placeholder functionality for demonstration
+  // In a live site, this could navigate to a new page or open more information
+  alert("Learn more about " + category);
+}
